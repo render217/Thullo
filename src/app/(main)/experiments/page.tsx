e040b1bcf7f488208527d30420c32fafc746e1ef
render <142716427+render217@users.noreply.github.com>
2024-08-21 22:25:52 +0300

    .

--- src/app/(main)/experiments/page.tsx
+++ src/app/(main)/experiments/page.tsx
@@ -1,17 +1,17 @@
 import { SignedIn, SignOutButton } from "@clerk/nextjs";
-import PageWrapper from "./_components/page-wrapper";
+// import PageWrapper from "./_components/page-wrapper";
 
 export default function page() {
   return (
-    <PageWrapper>
-      <div className="">
-        <p className="text-center text-2xl font-semibold text-white">Demo's</p>
-        <SignedIn>
-          <div className="mx-auto my-2 w-fit rounded-md bg-red-400 p-2 px-2 text-sm text-white">
-            <SignOutButton redirectUrl="/" />
-          </div>
-        </SignedIn>
-      </div>
-    </PageWrapper>
+    // <PageWrapper>
+    <div className="">
+      <p className="text-center text-2xl font-semibold text-white">Demo's</p>
+      <SignedIn>
+        <div className="mx-auto my-2 w-fit rounded-md bg-red-400 p-2 px-2 text-sm text-white">
+          <SignOutButton redirectUrl="/" />
+        </div>
+      </SignedIn>
+    </div>
+    // {/* </PageWrapper> */}
   );
 }
